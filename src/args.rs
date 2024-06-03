9afab34c747dac0d9901c6b974e2456dabea4d05
Alberto Elorza <albertoelorza2002@gmail.com>
2024-06-03 19:31:17 +0200

    added conditionally mounted worker through args

---
+++ src/args.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+  #[arg(short, long, default_value = "0.0.0.0:3000")]
+  pub bind: String,
+
+  #[arg(long, default_value_t = false, help = "expose the /worker endpoint")]
+  pub edit_workers: bool
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::Router;
+use clap::Parser;
 use services::worker::WorkerService;
 use tower_http::cors::CorsLayer;
 use tracing::{info, Level};
@@ -6,9 +7,12 @@ use tracing::{info, Level};
 mod model;
 mod routes;
 mod services;
+mod args;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+  let args = args::Args::parse();
+
   let subscriber = tracing_subscriber::fmt::fmt().with_max_level(Level::TRACE).finish();
   tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
@@ -16,12 +20,14 @@ async fn main() -> anyhow::Result<()> {
 
   let worker_service = WorkerService::new("./workers/")?;
 
-  let app = Router::new()
-    .nest("/task", routes::task::router(&worker_service))
-    .nest("/worker", routes::worker::router(&worker_service))
-    .layer(cors);
+  let mut app = Router::new()
+    .nest("/task", routes::task::router(&worker_service));
+  if args.edit_workers {
+    app = app.nest("/worker", routes::worker::router(&worker_service));
+  }
+  app = app.layer(cors);
 
-  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  let listener = tokio::net::TcpListener::bind(args.bind).await.unwrap();
 
   info!("server started");
 
